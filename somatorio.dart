f1cf1b982e5fb9eade87e578265a581dd21ac849
EmersonEdinaldo <emersonedinaldosl@gmail.com>
2022-08-23 14:55:53 -0300

    atualização do código

--- somatorio.dart
+++ somatorio.dart
@@ -1,12 +1,16 @@
 void main() {
-  var num = 11;
+  somatorio(10);
+}
+
+int somatorio(int num) {
   var soma = 0;
 
-  for (var i = 1; i < num; i++) {
+  for (int i = 1; i < num; i++) {
     if (i % 3 == 0 || (i % 5 == 0)) {
       print(i);
       soma += i;
     }
   }
   print(soma);
+  return soma;
 }
